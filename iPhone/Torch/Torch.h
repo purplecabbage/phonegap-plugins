e7dc9ac896c0f64befbce7ca52e0dabb9de1bed1
Shazron Abdullah <shazron@gmail.com>
2011-06-29 06:14:59 -0700

    Re-fix: imports need to be changed as well for PGPlugin

--- iPhone/Torch/Torch.h
+++ iPhone/Torch/Torch.h
@@ -8,9 +8,9 @@
 #import <AVFoundation/AVFoundation.h>
 
 #ifdef PHONEGAP_FRAMEWORK
-#import <PhoneGap/PhoneGapCommand.h>
+#import <PhoneGap/PGPlugin.h>
 #else
-#import "PhoneGapCommand.h"
+#import "PGPlugin.h"
 #endif
 
 @interface Torch : PGPlugin {
