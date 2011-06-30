7a9164a17090dbca44f7cf698cba4b30511fa156
Paul Panserrieu <ppa@zenexity.com>
2011-06-30 17:21:06 +0200

    Adding an ios Prompt plugin

---
+++ iPhone/Prompt/Prompt.h
@@ -0,0 +1,31 @@
+//	Phonegap Prompt Plugin
+//	Copyright (c) Paul Panserrieu, Zenexity 2011
+//	MIT Licensed
+
+#import <Foundation/Foundation.h>
+#import <PhoneGap/PhoneGapCommand.h>
+
+@interface PromptAlertView : UIAlertView {
+    UITextField *textField;
+    NSString* callback;
+}
+
+@property (nonatomic, retain) UITextField *textField;
+@property (readonly) NSString *enteredText;
+@property (nonatomic, copy) NSString* callback;
+
+- (id)initWithTitle : (NSString *) title
+             delegate : (id) delegate
+             cancelButtonTitle : (NSString *) cancelButtonTitle
+             okButtonTitle : (NSString *) okButtonTitle;
+
+- (NSString *)getCallback;
+@end
+
+@interface Prompt : PhoneGapCommand {
+
+}
+
+- (void) show:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
+@end
+
