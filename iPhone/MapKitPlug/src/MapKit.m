f92b520f4f5fc7db368d5953e09024724d94953a
shazron <shazron@gmail.com>
2011-07-11 18:56:15 -0700

    Updated iOS MapKit plugin to support 1.0.0rc1

--- iPhone/MapKitPlug/src/MapKit.m
+++ iPhone/MapKitPlug/src/MapKit.m
@@ -118,7 +118,7 @@
 	SBJSON *parser=[[SBJSON alloc] init];
 	NSArray *pins = [parser objectWithString:[arguments objectAtIndex:0]];
 	[parser autorelease];
-	CGRect webViewBounds = webView.bounds;
+	CGRect webViewBounds = self.webView.bounds;
 	
 	CGRect mapBounds;
 	if (atBottom) 
@@ -181,7 +181,7 @@
 {
 	[ self hideMap:NULL withDict:NULL];
 	NSString* jsString = [NSString stringWithFormat:@"%@(\"%i\");", self.buttonCallback,-1];
-	[webView stringByEvaluatingJavaScriptFromString:jsString];
+	[self.webView stringByEvaluatingJavaScriptFromString:jsString];
 }
 
 - (void)showMap:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options
@@ -277,7 +277,7 @@
 {
 	UIButton *tmpButton = button;
 	NSString* jsString = [NSString stringWithFormat:@"%@(\"%i\");", self.buttonCallback, tmpButton.tag];
-	[webView stringByEvaluatingJavaScriptFromString:jsString];
+	[self.webView stringByEvaluatingJavaScriptFromString:jsString];
 }
 
 - (void)dealloc
