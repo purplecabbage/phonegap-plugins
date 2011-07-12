b004a2fbe5554621ff5905599ab720d63865ada5
Kevin Schaul <kevin.schaul@gmail.com>
2011-07-11 21:27:10 -0700

    Added Share plugin for Android

---
+++ Android/Share/Share.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ * Phonegap share plugin for Android
+ * Kevin Schaul 2011
+ *
+ */
+
+package com.schaul.plugins.share;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Intent;
+
+import com.phonegap.api.Plugin;
+import com.phonegap.api.PluginResult;
+
+public class Share extends Plugin {
+
+	@Override
+	public PluginResult execute(String action, JSONArray args, String callbackId) {
+		try {
+			JSONObject jo = args.getJSONObject(0);
+			doSendIntent(jo.getString("subject"), jo.getString("text")); 
+			return new PluginResult(PluginResult.Status.OK);
+		} catch (JSONException e) {
+			return new PluginResult(PluginResult.Status.JSON_EXCEPTION);
+		}
+	}
+	
+	private void doSendIntent(String subject, String text) {
+		Intent sendIntent = new Intent(android.content.Intent.ACTION_SEND);
+		sendIntent.setType("text/plain");
+		sendIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+		sendIntent.putExtra(android.content.Intent.EXTRA_TEXT, text);
+		this.ctx.startActivity(sendIntent);
+	}
+
+}
