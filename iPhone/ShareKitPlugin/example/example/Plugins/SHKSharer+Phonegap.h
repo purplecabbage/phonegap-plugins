5bcb78a99297c7a3994d9e787a3e66adc6cbeb1a
Erick <ecamacho.mx@gmail.com>
2011-08-13 22:07:49 -0500

    added new method for Facebook login and Facebook share

---
+++ iPhone/ShareKitPlugin/example/example/Plugins/SHKSharer+Phonegap.h
@@ -0,0 +1,16 @@
+//
+//  SHKSharer+Phonegap.h
+//  example
+//
+//  Created by Erick Camacho Chavarría on 13/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SHKSharer.h"
+
+@interface SHKSharer (SHKSharer_Phonegap)
+
+//Shows only the login form of the service without showing the share form
++ (id)loginToService;
+
+@end
