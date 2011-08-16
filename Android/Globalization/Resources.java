65bbf836e032c367e7df8b6e4b3bb44f1af52f71
Bryce Curtis <curtis.bryce@gmail.com>
2011-08-16 11:14:44 -0500

    Add Globalization Plugin for Android.

---
+++ Android/Globalization/Resources.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.phonegap.plugins.globalization;
+
+/**
+ * @author costanzo
+ *
+ */
+public class Resources {
+	//GlobalizationCommand Plugin Actions
+	public static final String GETLOCALENAME = "getLocaleName";
+	public static final String DATETOSTRING = "dateToString";
+	public static final String STRINGTODATE = "stringToDate";
+	public static final String GETDATEPATTERN = "getDatePattern";
+	public static final String GETDATENAMES = "getDateNames";
+	public static final String ISDAYLIGHTSAVINGSTIME = "isDayLightSavingsTime";
+	public static final String GETFIRSTDAYOFWEEK = "getFirstDayOfWeek";
+	public static final String NUMBERTOSTRING = "numberToString";
+	public static final String STRINGTONUMBER = "stringToNumber";
+	public static final String GETNUMBERPATTERN = "getNumberPattern";
+	public static final String GETCURRENCYPATTERN = "getCurrencyPattern";
+	
+	//GlobalizationCommand Option Parameters
+	public static final String OPTIONS = "options";
+	public static final String FORMATLENGTH = "formatLength";
+	//public static final String SHORT = "short"; //default for dateToString format
+	public static final String MEDIUM = "medium";
+	public static final String LONG = "long";
+	public static final String FULL = "full";
+	public static final String SELECTOR = "selector";
+	//public static final String DATEANDTIME = "date and time"; //default for dateToString
+	public static final String DATE = "date";
+	public static final String TIME = "time";
+	public static final String DATESTRING = "dateString";
+	public static final String TYPE = "type";
+	public static final String ITEM = "item";
+	public static final String NARROW = "narrow";
+	public static final String WIDE = "wide";
+	public static final String MONTHS = "months";
+	public static final String DAYS = "days";
+	//public static final String DECMIAL = "wide"; //default for numberToString
+	public static final String NUMBER = "number";
+	public static final String NUMBERSTRING = "numberString";
+	public static final String PERCENT = "percent";
+	public static final String CURRENCY = "currency";
+	public static final String CURRENCYCODE = "currencyCode";
+
+	
+}
