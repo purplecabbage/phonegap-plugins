aab6c58f5a6aabe8a65ee74fbb1d8804396a1a52
macdonst <simon.macdonald@gmail.com>
2011-08-26 09:58:01 -0400

    ChildBrowser using dialog approach

--- Android/ChildBrowser/ChildBrowser.java
+++ Android/ChildBrowser/ChildBrowser.java
@@ -2,21 +2,35 @@
  * PhoneGap is available under *either* the terms of the modified BSD license *or* the
  * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
  *
- * Copyright (c) 2005-2010, Nitobi Software Inc.
- * Copyright (c) 2010, IBM Corporation
+ * Copyright (c) 2005-2011, Nitobi Software Inc.
+ * Copyright (c) 2010-2011, IBM Corporation
  */
 package com.phonegap.plugins.childBrowser;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
+import com.ibm.filcontact.R;
+import com.phonegap.api.PhonegapActivity;
 import com.phonegap.api.Plugin;
 import com.phonegap.api.PluginResult;
 
 public class ChildBrowser extends Plugin {
+    
+    private Dialog dialog;
+    private WebView webview;
 
     /**
      * Executes the request and returns PluginResult.
@@ -32,7 +46,16 @@ public class ChildBrowser extends Plugin {
 
         try {
             if (action.equals("showWebPage")) {
-                result = this.showWebPage(args.getString(0), args.optBoolean(1));
+                result = this.showWebPage(args.getString(0));
+                if (result.length() > 0) {
+                    status = PluginResult.Status.ERROR;
+                }
+            }
+            else if (action.equals("close")) {
+                closeDialog();
+            }
+            else if (action.equals("openExternal")) {
+                result = this.openExternal(args.getString(0), args.optBoolean(1));
                 if (result.length() > 0) {
                     status = PluginResult.Status.ERROR;
                 }
@@ -44,45 +67,16 @@ public class ChildBrowser extends Plugin {
     }
 
     /**
-     * Identifies if action to be executed returns a value and should be run synchronously.
-     *
-     * @param action    The action to execute
-     * @return          T=returns value
-     */
-    public boolean isSynch(String action) {
-        return false;
-    }
-
-    /**
-     * Called by AccelBroker when listener is to be shut down.
-     * Stop listener.
-     */
-    public void onDestroy() {
-    }
-
-    //--------------------------------------------------------------------------
-    // LOCAL METHODS
-    //--------------------------------------------------------------------------
-
-    /**
      * Display a new browser with the specified URL.
-     * 
-     * NOTE: If usePhoneGap is set, only trusted PhoneGap URLs should be loaded,
-     *       since any PhoneGap API can be called by the loaded HTML page.
      *
      * @param url           The url to load.
-     * @param usePhoneGap   Load url in PhoneGap webview.
+     * @param usePhoneGap   Load url in PhoneGap webview
      * @return              "" if ok, or error message.
      */
-    public String showWebPage(String url, boolean usePhoneGap) {
+    public String openExternal(String url, boolean usePhoneGap) {
         try {
             Intent intent = null;
             if (usePhoneGap) {
-            	// Loads and displays a new PhoneGap app on top of current PhoneGap app.
-            	// For the currently running PhoneGap app:
-            	// 		If keepRunning=true (default), then the current app continues to run in background
-            	// 		If keepRunning=false, then the current app is paused by Android
-            	// When BACK is pressed, the current app has focus.
                 intent = new Intent().setClass(this.ctx, com.phonegap.DroidGap.class);
                 intent.setData(Uri.parse(url)); // This line will be removed in future.
                 intent.putExtra("url", url);
@@ -93,7 +87,6 @@ public class ChildBrowser extends Plugin {
                 // These parameters can be configured if you want to show the loading dialog
                 intent.putExtra("loadingDialog", "Wait,Loading web page...");   // show loading dialog
                 intent.putExtra("hideLoadingDialogOnPageLoad", true);           // hide it once page has completely loaded
-                
             }
             else {
                 intent = new Intent(Intent.ACTION_VIEW);
@@ -102,9 +95,119 @@ public class ChildBrowser extends Plugin {
             this.ctx.startActivity(intent);
             return "";
         } catch (android.content.ActivityNotFoundException e) {
-            System.out.println("ChildBrowser: Error loading url "+url+":"+ e.toString());
+            System.out.println("ChildDialog: Error loading url "+url+":"+ e.toString());
             return e.toString();
         }
     }
 
+    /**
+     * Closes the dialog
+     */
+    private void closeDialog() {
+        if (dialog != null) {
+            dialog.dismiss();
+        }
+    }
+
+    /**
+     * Checks to see if it is possible to go back one page in history, then does so.
+     */
+    private void goBack() {
+        if (this.webview.canGoBack()) {
+            this.webview.goBack();
+        }
+    }
+
+    /**
+     * Checks to see if it is possible to go forward one page in history, then does so.
+     */
+    private void goForward() {
+        if (this.webview.canGoForward()) {
+            this.webview.goForward();
+        }
+    }
+
+    /**
+     * Display a new browser with the specified URL.
+     *
+     * @param url           The url to load.
+     */
+    public String showWebPage(final String url) {
+        Runnable runnable = new Runnable() {
+            public void run() {
+                /**/
+                dialog = new Dialog(ctx);
+
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.childbrowser);
+                dialog.setTitle("Child Browser");
+                dialog.setCancelable(true);
+                dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        public void onDismiss(DialogInterface dialog) {
+                            ctx.sendJavascript("window.plugins.childDialog._onClose();");
+                        }
+                });
+
+                Button back = (Button) dialog.findViewById(R.id.backBtn);
+                back.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        goBack();
+                    }
+                });
+                Button forward = (Button) dialog.findViewById(R.id.forwardBtn);
+                forward.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        goForward();
+                    }
+                });
+                Button button = (Button) dialog.findViewById(R.id.closeBtn);
+                button.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        closeDialog();
+                    }
+                });
+
+                webview = (WebView) dialog.findViewById(R.id.childBrowser);
+                webview.getSettings().setJavaScriptEnabled(true);
+                WebViewClient client = new ChildBrowserClient(ctx);
+                webview.setWebViewClient(client);                
+                webview.loadUrl(url);
+                
+                dialog.show();
+            }
+        };
+        this.ctx.runOnUiThread(runnable);
+        return "";
+    }
+
+    /**
+     * The webview client receives notifications about appView
+     */
+    public class ChildBrowserClient extends WebViewClient {
+
+        private static final String LOG_TAG = "ChildBrowser";
+        PhonegapActivity ctx;
+
+        /**
+         * Constructor.
+         * 
+         * @param mContext
+         */
+        public ChildBrowserClient(PhonegapActivity mContext) {
+            this.ctx = mContext;
+        }       
+
+        /**
+         * Notify the host application that a page has started loading.
+         * 
+         * @param view          The webview initiating the callback.
+         * @param url           The url of the page.
+         */
+        @Override
+        public void onPageStarted(WebView view, String url,  Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            Log.d(LOG_TAG, "Hey I got a page started for url = " + url);
+            ctx.sendJavascript("window.plugins.childDialog._onLocationChange('" + url + "');");
+        }
+    }
 }
--- Android/ChildBrowser/childbrowser.js
+++ Android/ChildBrowser/childbrowser.js
@@ -14,15 +14,49 @@ function ChildBrowser() {
 
 /**
  * Display a new browser with the specified URL.
- * 
- * NOTE: If usePhoneGap is set, only trusted PhoneGap URLs should be loaded,
- *       since any PhoneGap API can be called by the loaded HTML page.
+ * This method loads up a new web view in a dialog.
  *
  * @param url           The url to load
- * @param usePhoneGap   Load url in PhoneGap webview [optional] - Default: false
  */
-ChildBrowser.prototype.showWebPage = function(url, usePhoneGap) {
-    PhoneGap.exec(null, null, "ChildBrowser", "showWebPage", [url, usePhoneGap]);
+ChildBrowser.prototype.showWebPage = function(url) {
+    PhoneGap.exec(null, null, "ChildBrowser", "showWebPage", [url]);
+};
+
+/**
+ * Close the browser opened by showWebPage.
+ */
+ChildBrowser.prototype.close = function() {
+    PhoneGap.exec(null, null, "ChildBrowser", "close", []);
+};
+
+/**
+ * Display a new browser with the specified URL.
+ * This method starts a new web browser activity.
+ *
+ * @param url           The url to load
+ * @param usePhoneGap   Load url in PhoneGap webview [optional]
+ */
+ChildBrowser.prototype.openExternal = function(url, usePhoneGap) {
+    PhoneGap.exec(null, null, "ChildBrowser", "openExternal", [url, usePhoneGap]);
+};
+
+/**
+ * Method called when the child browser is closed.
+ */
+ChildBrowser.prototype._onClose = function() {
+    if (typeof window.plugins.ChildBrowser.onClose === "function") {
+        window.plugins.ChildBrowser.onClose();
+    }
+};
+
+/**
+ * Method called when the child browser location changes.
+ * @param {DOMString} newLoc the url that is being loaded
+ */
+ChildBrowser.prototype._onLocationChange = function(newLoc) {
+    if (typeof window.plugins.ChildBrowser.onLocationChange === "function") {
+        window.plugins.ChildBrowser.onLocationChange(newLoc);
+    }
 };
 
 /**
@@ -30,7 +64,4 @@ ChildBrowser.prototype.showWebPage = function(url, usePhoneGap) {
  */
 PhoneGap.addConstructor(function() {
     PhoneGap.addPlugin("childBrowser", new ChildBrowser());
-// @deprecated: No longer needed in PhoneGap 1.0. Uncomment the addService code for earlier 
-// PhoneGap releases.
-//    PluginManager.addService("ChildBrowser", "com.phonegap.plugins.childBrowser.ChildBrowser");
 });
