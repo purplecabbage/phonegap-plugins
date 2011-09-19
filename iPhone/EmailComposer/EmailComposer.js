11d252f3510c81e1fad20190d768ca4a1df167b5
dsowsy <dsowsy@gmail.com>
2011-09-19 18:42:46 -0300

    Edited iPhone/EmailComposer/EmailComposer.js via GitHub

--- iPhone/EmailComposer/EmailComposer.js
+++ iPhone/EmailComposer/EmailComposer.js
@@ -1,5 +1,67 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
 // window.plugins.emailComposer
-													  
+
 function EmailComposer()
 {
 	this.resultCallback = null; // Function
@@ -34,7 +96,7 @@ EmailComposer.prototype.showEmailComposer = function(subject,body,toRecipients,c
 	if(bIsHTML)
 		args.bIsHTML = bIsHTML;
 
-	PhoneGap.exec("EmailComposer.showEmailComposer",args);
+	PhoneGap.exec("com.phonegap.emailComposer.showEmailComposer",args);
 }
 
 // this will be forever known as the orch-func -jm
