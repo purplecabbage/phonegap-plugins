901507db491c03017eb0095e5e1bec9b3ee601e7
macdonst <simon.macdonald@gmail.com>
2011-10-13 13:07:18 -0400

    Update copyright statements

--- Android/BarcodeScanner/assets/www/barcodescanner.js
+++ Android/BarcodeScanner/assets/www/barcodescanner.js
@@ -1,7 +1,8 @@
-/*
+/**
  * PhoneGap is available under *either* the terms of the modified BSD license *or* the
  * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
- * 
+ *
+ * Copyright (c) Matt Kane 2010
  * Copyright (c) 2011, IBM Corporation
  */
 
