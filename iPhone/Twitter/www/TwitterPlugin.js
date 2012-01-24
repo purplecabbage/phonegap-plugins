8c654dd132eceb6231815f2020b6ae63e43b09fe
Brian Antonelli <brian.antonelli@gmail.com>
2012-01-23 23:42:03 -0500

    Reworking compose tweet
    Merged sendTweet and composeTweet. Changed how optional parameters
    (image and url attachments) are sent. Added additional examples for
    creating tweets.

--- iPhone/Twitter/www/TwitterPlugin.js
+++ iPhone/Twitter/www/TwitterPlugin.js
@@ -8,15 +8,10 @@ Twitter.prototype.isTwitterSetup = function(response){
     PhoneGap.exec(response, null, "com.phonegap.twitter", "isTwitterSetup", []);
 };
 
-Twitter.prototype.sendTweet = function(success, failure, tweetText, urlAttach, imageAttach){
-    if(typeof urlAttach === "undefined") urlAttach = "";
-    if(typeof imageAttach === "undefined") imageAttach = "";
-    
-    PhoneGap.exec(success, failure, "com.phonegap.twitter", "sendTweet", [tweetText, urlAttach, imageAttach]);
-};
-
-Twitter.prototype.composeTweet = function() {
-    PhoneGap.exec(null, null, "com.phonegap.twitter", "composeTweet", []);
+Twitter.prototype.composeTweet = function(success, failure, tweetText, options){
+    options = options || {};
+    options.text = tweetText;
+    PhoneGap.exec(success, failure, "com.phonegap.twitter", "composeTweet", [options]);
 };
 
 Twitter.prototype.getPublicTimeline = function(success, failure){
