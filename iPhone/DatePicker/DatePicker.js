76a724e312d32fd52a21397d7ed7717a02b1bcf6
Sam de Freyssinet <sam@def.reyssi.net>
2012-02-22 15:48:33 -0600

    More refactoring of the DatePicker implmentation. Created one init method for the ActionSheet that accepts its dependencies via DI. All date handling is now internationally recognised ISO. DatePicker will default to UIDatePickerModeDateAndTime unless specified otherwise. All memory is managed properly, making DatePicker plugin play nice in iOS (without ARC)

--- iPhone/DatePicker/DatePicker.js
+++ iPhone/DatePicker/DatePicker.js
@@ -15,11 +15,22 @@ if (typeof PhoneGap !== "undefined") {
      * show - true to show the ad, false to hide the ad
      */
     DatePicker.prototype.show = function(options, cb) {
+        var padDate = function(date) {
+          if (date.length == 1) {
+            return ("0" + date);
+          }
+          return date;
+        };
+
         if (options.date) {
-            options.date = (options.date.getMonth()+1)+"/"+(options.date.getDate())+"/"+(options.date.getFullYear());
+            options.date = options.date.getFullYear() + "-" +
+                           padDate(options.date.getMonth()+1) + "-" +
+                           padDate(options.date.getDate()) +
+                           "T" + padDate(options.date.getHours()) + ":" +
+                           padDate(options.date.getMinutes()) + ":00Z";
         }
         var defaults = {
-            mode: '',
+            mode: 'datetime',
             date: '',
             allowOldDates: true
         }
