3118ddc85825d70068918ba8fbd74533796c77ef
Vishal Rajpal <vishalrajpal.apiit@gmail.com>
2012-04-04 11:21:52 +0530

    Changes for PhoneGap

--- Android/ExtractZipFile/www/ZipPlugin.js
+++ Android/ExtractZipFile/www/ZipPlugin.js
@@ -1,7 +1,8 @@
 /*
  	Author: Vishal Rajpal
  	Filename: ZipPlugin.js
- 	Date: 21-02-2012
+ 	Created Date: 21-02-2012
+ 	Modified Date: 04-04-2012
 */
 
 var ExtractZipFilePlugin=function(){
@@ -9,11 +10,10 @@ var ExtractZipFilePlugin=function(){
 
 PhoneGap.addConstructor(function() 
 {
-	PhoneGap.addPlugin('ExtractZipFilePlugin', new ZipPlugin());
+	PhoneGap.addPlugin('ExtractZipFilePlugin', new ExtractZipFilePlugin());
 });
 
 ExtractZipFilePlugin.prototype.extractFile = function(file, successCallback, errorCallback) 
 {
-	alert(file);
     return PhoneGap.exec(successCallback, errorCallback, "ZipPlugin", "extract", [file]);
 };
