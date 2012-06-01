030b3782e83b9bd12c432638edf1ec48103d71fc
Trevor Suarez <Rican7@gmail.com>
2012-06-01 09:43:53 -0400

    Removed the url text edit ability. More like the iOS ChildBrowser plugin now. Plus, we don't want them just roaming around using it as a browser anyway.

--- Android/ChildBrowser/src/com/phonegap/plugins/childBrowser/ChildBrowser.java
+++ Android/ChildBrowser/src/com/phonegap/plugins/childBrowser/ChildBrowser.java
@@ -314,6 +314,7 @@ public class ChildBrowser extends Plugin {
                 edittext.setText(url);
                 edittext.setInputType(InputType.TYPE_TEXT_VARIATION_URI);
                 edittext.setImeOptions(EditorInfo.IME_ACTION_GO);
+                edittext.setInputType(InputType.TYPE_NULL); // Will not except input... Makes the text NON-EDITABLE
                 edittext.setOnKeyListener(new View.OnKeyListener() {
                     public boolean onKey(View v, int keyCode, KeyEvent event) {
                         // If the event is a key-down event on the "enter" button
