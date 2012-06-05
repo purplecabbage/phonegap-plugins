4c2ec54ae001ad409e4d4fe6f5ec6b9ef8b7dc3f
Josemando Sobral <josemando@gmail.com>
2012-06-05 16:33:05 -0300

    updated Screenshot plugin to Cordova 1.6.x

---
+++ iOS/Screenshot/Screenshot.m
@@ -0,0 +1,52 @@
+//
+//  Screenshot.h
+//
+//  Created by Simon Madine on 29/04/2010.
+//  Copyright 2010 The Angry Robot Zombie Factory.
+//   - Converted to Cordova 1.6.1 by Josemando Sobral.
+//  MIT licensed
+//
+//  Modifications to support orientation change by @ffd8
+//
+
+#import "Screenshot.h"
+
+@implementation Screenshot
+
+@synthesize webView;
+
+- (void)saveScreenshot:(NSArray*)arguments withDict:(NSDictionary*)options
+{
+	CGRect imageRect;
+	CGRect screenRect = [[UIScreen mainScreen] bounds];
+
+	// statusBarOrientation is more reliable than UIDevice.orientation
+	UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+
+	if (orientation == UIInterfaceOrientationLandscapeLeft || orientation == UIInterfaceOrientationLandscapeRight) { 
+		// landscape check
+		imageRect = CGRectMake(0, 0, CGRectGetHeight(screenRect), CGRectGetWidth(screenRect));
+	} else {
+		// portrait check
+		imageRect = CGRectMake(0, 0, CGRectGetWidth(screenRect), CGRectGetHeight(screenRect));
+	}
+
+	UIGraphicsBeginImageContext(imageRect.size);
+
+	CGContextRef ctx = UIGraphicsGetCurrentContext();
+	[[UIColor blackColor] set];
+	CGContextTranslateCTM(ctx, 0, 0);
+	CGContextFillRect(ctx, imageRect);
+
+	[webView.layer renderInContext:ctx];
+
+	UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+	UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+	UIGraphicsEndImageContext();
+
+	UIAlertView *alert= [[UIAlertView alloc] initWithTitle:nil message:@"Image Saved" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+	[alert show];
+	[alert release];
+}
+
+@end
