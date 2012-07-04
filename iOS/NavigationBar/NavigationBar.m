c306d69d74b692d795e22ca607371e70df70a7f0
Andreas Sommer <andreas.sommer87@googlemail.com>
2012-07-04 22:48:26 +0100

    Final fixes for NavigationBar plugin

--- iOS/NavigationBar/NavigationBar.m
+++ iOS/NavigationBar/NavigationBar.m
@@ -8,7 +8,7 @@
     (AndiDog, https://github.com/AndiDog/phonegap-plugins).
 */
 
-#import "NativeControls.h"
+#import "NavigationBar.h"
 #import <QuartzCore/QuartzCore.h>
 
 #ifdef CORDOVA_FRAMEWORK
@@ -17,21 +17,18 @@
 #import "CDVDebug.h"
 #endif
 
-@implementation NativeControls
+@implementation NavigationBar
 #ifndef __IPHONE_3_0
 @synthesize webView;
 #endif
 @synthesize navBarController;
 
-
 -(CDVPlugin*) initWithWebView:(UIWebView*)theWebView
 {
-    self = (NativeControls*)[super initWithWebView:theWebView];
+    self = (NavigationBar*)[super initWithWebView:theWebView];
     if (self)
 	{
-        tabBarItems = [[NSMutableDictionary alloc] initWithCapacity:5];
 		originalWebViewBounds = theWebView.bounds;
-        tabBarHeight = 49.0f;
         navBarHeight = 44.0f;
     }
     return self;
@@ -39,9 +36,6 @@
 
 - (void)dealloc
 {
-    if (tabBar)
-        [tabBar release];
-
     if (navBar)
         [navBar release];
 
@@ -84,161 +78,9 @@
     [self.webView setFrame:webViewBounds];
 }
 
-/**
- * Create a new tab bar item for use on a previously created tab bar.  Use ::showTabBarItems to show the new item on the tab bar.
- *
- * If the supplied image name is one of the labels listed below, then this method will construct a tab button
- * using the standard system buttons.  Note that if you use one of the system images, that the \c title you supply will be ignored.
- * - <b>Tab Buttons</b>
- *   - tabButton:More
- *   - tabButton:Favorites
- *   - tabButton:Featured
- *   - tabButton:TopRated
- *   - tabButton:Recents
- *   - tabButton:Contacts
- *   - tabButton:History
- *   - tabButton:Bookmarks
- *   - tabButton:Search
- *   - tabButton:Downloads
- *   - tabButton:MostRecent
- *   - tabButton:MostViewed
- * @brief create a tab bar item
- * @param arguments Parameters used to create the tab bar
- *  -# \c name internal name to refer to this tab by
- *  -# \c title title text to show on the tab, or null if no text should be shown
- *  -# \c image image filename or internal identifier to show, or null if now image should be shown
- *  -# \c tag unique number to be used as an internal reference to this button
- * @param options Options for customizing the individual tab item
- *  - \c badge value to display in the optional circular badge on the item; if nil or unspecified, the badge will be hidden
- */
-- (void)createTabBarItem:(NSArray*)arguments withDict:(NSDictionary*)options
-{
-    if (!tabBar)
-        [self createTabBar:nil withDict:nil];
-
-    NSString  *name      = [arguments objectAtIndex:0];
-    NSString  *title     = [arguments objectAtIndex:1];
-    NSString  *imageName = [arguments objectAtIndex:2];
-    int tag              = [[arguments objectAtIndex:3] intValue];
-
-    UITabBarItem *item = nil;
-    if ([imageName length] > 0) {
-        UITabBarSystemItem systemItem = -1;
-        if ([imageName isEqualToString:@"tabButton:More"])       systemItem = UITabBarSystemItemMore;
-        if ([imageName isEqualToString:@"tabButton:Favorites"])  systemItem = UITabBarSystemItemFavorites;
-        if ([imageName isEqualToString:@"tabButton:Featured"])   systemItem = UITabBarSystemItemFeatured;
-        if ([imageName isEqualToString:@"tabButton:TopRated"])   systemItem = UITabBarSystemItemTopRated;
-        if ([imageName isEqualToString:@"tabButton:Recents"])    systemItem = UITabBarSystemItemRecents;
-        if ([imageName isEqualToString:@"tabButton:Contacts"])   systemItem = UITabBarSystemItemContacts;
-        if ([imageName isEqualToString:@"tabButton:History"])    systemItem = UITabBarSystemItemHistory;
-        if ([imageName isEqualToString:@"tabButton:Bookmarks"])  systemItem = UITabBarSystemItemBookmarks;
-        if ([imageName isEqualToString:@"tabButton:Search"])     systemItem = UITabBarSystemItemSearch;
-        if ([imageName isEqualToString:@"tabButton:Downloads"])  systemItem = UITabBarSystemItemDownloads;
-        if ([imageName isEqualToString:@"tabButton:MostRecent"]) systemItem = UITabBarSystemItemMostRecent;
-        if ([imageName isEqualToString:@"tabButton:MostViewed"]) systemItem = UITabBarSystemItemMostViewed;
-        if (systemItem != -1)
-            item = [[UITabBarItem alloc] initWithTabBarSystemItem:systemItem tag:tag];
-    }
-
-    if (item == nil) {
-        item = [[UITabBarItem alloc] initWithTitle:title image:[UIImage imageNamed:imageName] tag:tag];
-    }
-
-    if ([options objectForKey:@"badge"])
-        item.badgeValue = [options objectForKey:@"badge"];
-
-    [tabBarItems setObject:item forKey:name];
-	[item release];
-}
-
-
-/**
- * Update an existing tab bar item to change its badge value.
- * @brief update the badge value on an existing tab bar item
- * @param arguments Parameters used to identify the tab bar item to update
- *  -# \c name internal name used to represent this item when it was created
- * @param options Options for customizing the individual tab item
- *  - \c badge value to display in the optional circular badge on the item; if nil or unspecified, the badge will be hidden
- */
-- (void)updateTabBarItem:(NSArray*)arguments withDict:(NSDictionary*)options
-{
-    if (!tabBar)
-        [self createTabBar:nil withDict:nil];
-
-    NSString  *name = [arguments objectAtIndex:0];
-    UITabBarItem *item = [tabBarItems objectForKey:name];
-    if (item)
-        item.badgeValue = [options objectForKey:@"badge"];
-}
-
-
-/**
- * Show previously created items on the tab bar
- * @brief show a list of tab bar items
- * @param arguments the item names to be shown
- * @param options dictionary of options, notable options including:
- *  - \c animate indicates that the items should animate onto the tab bar
- * @see createTabBarItem
- * @see createTabBar
- */
-- (void)showTabBarItems:(NSArray*)arguments withDict:(NSDictionary*)options
-{
-    if (!tabBar)
-        [self createTabBar:nil withDict:nil];
-
-    int i, count = [arguments count];
-    NSMutableArray *items = [[NSMutableArray alloc] initWithCapacity:count];
-    for (i = 0; i < count; i++) {
-        NSString *itemName = [arguments objectAtIndex:i];
-        UITabBarItem *item = [tabBarItems objectForKey:itemName];
-        if (item)
-            [items addObject:item];
-    }
-
-    BOOL animateItems = NO;
-    if ([options objectForKey:@"animate"])
-        animateItems = [(NSString*)[options objectForKey:@"animate"] boolValue];
-    [tabBar setItems:items animated:animateItems];
-	[items release];
-
-}
-
-/**
- * Manually select an individual tab bar item, or nil for deselecting a currently selected tab bar item.
- * @brief manually select a tab bar item
- * @param arguments the name of the tab bar item to select
- * @see createTabBarItem
- * @see showTabBarItems
- */
-- (void)selectTabBarItem:(NSArray*)arguments withDict:(NSDictionary*)options
-{
-    if (!tabBar)
-        [self createTabBar:nil withDict:nil];
-
-    NSString *itemName = [arguments objectAtIndex:0];
-    UITabBarItem *item = [tabBarItems objectForKey:itemName];
-    if (item)
-        tabBar.selectedItem = item;
-    else
-        tabBar.selectedItem = nil;
-}
-
-
-- (void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item
-{
-    NSString * jsCallBack = [NSString stringWithFormat:@"window.plugins.nativeControls.tabBarItemSelected(%d);", item.tag];
-    [self.webView stringByEvaluatingJavaScriptFromString:jsCallBack];
-}
-
-#pragma mark -
-#pragma mark navBar
-
-
-
-
 /*********************************************************************************/
 
--(void) createNavBar:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options
+-(void) create:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options
 {
     if (!navBar)
     {
@@ -246,13 +88,10 @@
         navBar = [navBarController view];
         [navBarController setDelegate:self];
 
-        NSLog(@"navBar width: %f",[navBar frame].size.width);
-        [[navBarController view] setFrame:CGRectMake(0, 0, originalWebViewBounds.size.width , navBarHeight)];
+        [[navBarController view] setFrame:CGRectMake(0, 0, originalWebViewBounds.size.width, navBarHeight)];
         [[[self webView] superview] addSubview:[navBarController view]];
         [navBar setHidden:YES];
-
     }
-
 }
 
 + (UIBarButtonSystemItem)getUIBarButtonSystemItemForString:(NSString*)imageName
@@ -299,12 +138,13 @@
     }
     else if (imageName && [imageName length] > 0)
     {
-        UIBarButtonSystemItem systemItem = [NativeControls getUIBarButtonSystemItemForString:imageName];
+        UIBarButtonSystemItem systemItem = [NavigationBar getUIBarButtonSystemItemForString:imageName];
 
         if (systemItem != -1)
         {
             UIBarButtonItem *newButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:systemItem target:self action:@selector(leftButtonTapped)];
             navBarController.navItem.leftBarButtonItem = newButton;
+            navBarController.leftButton = newButton;
             [newButton release];
             return;
         }
@@ -332,12 +172,13 @@
     }
     else if (imageName && [imageName length] > 0)
     {
-        UIBarButtonSystemItem systemItem = [NativeControls getUIBarButtonSystemItemForString:imageName];
+        UIBarButtonSystemItem systemItem = [NavigationBar getUIBarButtonSystemItemForString:imageName];
 
         if (systemItem != -1)
         {
             UIBarButtonItem *newButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:systemItem target:self action:@selector(rightButtonTapped)];
             navBarController.navItem.rightBarButtonItem = newButton;
+            navBarController.rightButton = newButton;
             [newButton release];
             return;
         }
@@ -375,20 +216,20 @@
 
 -(void) leftButtonTapped
 {
-    NSString * jsCallBack = @"window.plugins.nativeControls.leftButtonTapped();";
+    NSString * jsCallBack = @"window.plugins.navigationBar.leftButtonTapped();";
     [self.webView stringByEvaluatingJavaScriptFromString:jsCallBack];
 }
 
 -(void) rightButtonTapped
 {
-    NSString * jsCallBack = @"window.plugins.nativeControls.rightButtonTapped();";
+    NSString * jsCallBack = @"window.plugins.navigationBar.rightButtonTapped();";
     [self.webView stringByEvaluatingJavaScriptFromString:jsCallBack];
 }
 
--(void) showNavBar:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options
+-(void) show:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options
 {
     if (!navBar)
-        [self createNavBar:nil withDict:nil];
+        [self create:nil withDict:nil];
 
     if ([navBar isHidden])
     {
@@ -398,7 +239,7 @@
 }
 
 
--(void) hideNavBar:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options
+-(void) hide:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options
 {
     if (navBar && ![navBar isHidden])
     {
@@ -408,7 +249,7 @@
 
 }
 
--(void) setNavBarTitle:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options
+-(void) setTitle:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options
 {
     if (navBar)
     {
@@ -420,9 +261,8 @@
     }
 }
 
--(void) setNavBarLogo:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options
+-(void) setLogo:(NSMutableArray *)arguments withDict:(NSMutableDictionary *)options
 {
-
     NSString * logoURL = [arguments objectAtIndex:0];
     UIImage * image = nil;
 
@@ -434,22 +274,7 @@
             image = [UIImage imageWithData:data];
         }
         else
-        {
- /*           NSString * path = [HelloPhoneGapAppDelegate pathForResource:logoURL];
-            if (!path)
-            {
-                NSMutableArray *dirs = [NSMutableArray arrayWithArray:[logoURL componentsSeparatedByString:@"/"]];
-                NSString *filename = [dirs lastObject];
-                NSArray *nameParts = [filename componentsSeparatedByString:@"."];
-                path = [[NSBundle mainBundle] pathForResource:[nameParts objectAtIndex:0] ofType:[nameParts lastObject]];
-
-            }
-            if (path)
-            {
-                image = [UIImage imageWithContentsOfFile:path];
-            } */
-        }
-
+            image = [UIImage imageNamed:logoURL];
 
         if (image)
         {
@@ -459,52 +284,6 @@
             [[navBarController navItem] setTitleView:view];
         }
     }
-
-}
-
-
-#pragma mark -
-#pragma mark ActionSheet
-
-- (void)createActionSheet:(NSArray*)arguments withDict:(NSDictionary*)options
-{
-
-	NSString* title = [options objectForKey:@"title"];
-
-
-	UIActionSheet* actionSheet = [ [UIActionSheet alloc ]
-                                  initWithTitle:title
-                                  delegate:self
-                                  cancelButtonTitle:nil
-                                  destructiveButtonTitle:nil
-                                  otherButtonTitles:nil
-                                  ];
-
-	int count = [arguments count];
-	for(int n = 0; n < count; n++)
-	{
-		[ actionSheet addButtonWithTitle:[arguments objectAtIndex:n]];
-	}
-
-	if([options objectForKey:@"cancelButtonIndex"])
-	{
-		actionSheet.cancelButtonIndex = [[options objectForKey:@"cancelButtonIndex"] intValue];
-	}
-	if([options objectForKey:@"destructiveButtonIndex"])
-	{
-		actionSheet.destructiveButtonIndex = [[options objectForKey:@"destructiveButtonIndex"] intValue];
-	}
-
-	actionSheet.actionSheetStyle = UIActionSheetStyleBlackTranslucent;//UIActionSheetStyleBlackOpaque;
-    [actionSheet showInView:self.webView.superview];
-    [actionSheet release];
-
-}
-
-- (void)actionSheet:(UIActionSheet *)actionSheet didDismissWithButtonIndex:(NSInteger)buttonIndex
-{
-	NSString * jsCallBack = [NSString stringWithFormat:@"window.plugins.nativeControls._onActionSheetDismissed(%d);", buttonIndex];
-    [self.webView stringByEvaluatingJavaScriptFromString:jsCallBack];
 }
 
 @end
