1362a06aba1395c270807ad0b9daafc3591857f8
Braden Shepherdson <braden@chromium.org>
2012-08-30 16:10:41 -0400

    onclick for Android status bar notifications
    Cleans up the Android status bar notifications code and handles clicks
    on the notification, calling the onclick() callback defined on the
    notification in Javascript.

--- Android/StatusBarNotification/StatusNotificationIntent.java
+++ Android/StatusBarNotification/StatusNotificationIntent.java
@@ -10,16 +10,17 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 
 public class StatusNotificationIntent {
-    public static Notification buildNotification( Context context, CharSequence contentTitle, CharSequence contentText ) {
+    public static Notification buildNotification( Context context, CharSequence tag, CharSequence contentTitle, CharSequence contentText ) {
         int icon = R.drawable.notification;
         long when = System.currentTimeMillis();
         Notification noti = new Notification(icon, contentTitle, when);
         noti.flags |= Notification.FLAG_AUTO_CANCEL;
-        
+
         PackageManager pm = context.getPackageManager();
         Intent notificationIntent = pm.getLaunchIntentForPackage(context.getPackageName());
         notificationIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        
+        notificationIntent.putExtra("notificationTag", tag);
+
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
         noti.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
         return noti;
