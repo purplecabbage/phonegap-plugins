8812aba88fc3883b16e154a175135283af940db6
Simon MacDonald <simon.macdonald@gmail.com>
2012-11-08 16:49:08 -0500

    Move all my plugins

--- Android/IMEI/v1.4.1/imei.js
+++
@@ -1,9 +0,0 @@
-var IMEI = function(){};
-
-IMEI.prototype.get = function(onSuccess, onFail){
-    return PhoneGap.exec(onSuccess, onFail, 'IMEI', 'get', []);
-};
-
-PhoneGap.addConstructor(function(){
-    PhoneGap.addPlugin('imei', new IMEI());
-});
--- Android/IMEI/v1.5.0/imei.js
+++
@@ -1,9 +0,0 @@
-var IMEI = function(){};
-
-IMEI.prototype.get = function(onSuccess, onFail){
-    return cordova.exec(onSuccess, onFail, 'IMEI', 'get', []);
-};
-
-cordova.addConstructor(function(){
-    cordova.addPlugin('imei', new IMEI());
-});
