7d8f569b26211ce3637545614a1f8d5ba17ef0cb
Alexander Wunschik <alex@wunschik.net>
2012-11-18 18:26:16 +0100

    ChildBrowser iOS6 bugfix: setting audio session category to "Playback"
    based on a bugfix from Chris Lundie:
    http://stackoverflow.com/posts/12414719/revisions
    
    fixes #890

--- iOS/ChildBrowser/ChildBrowserCommand.m
+++ iOS/ChildBrowser/ChildBrowserCommand.m
@@ -4,6 +4,7 @@
 
 #import "ChildBrowserCommand.h"
 #import <Cordova/CDVViewController.h>
+#import <AVFoundation/AVFoundation.h>
 
 @implementation ChildBrowserCommand
 
@@ -11,6 +12,14 @@
 
 - (void)showWebPage:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options  // args: url
 {
+    /* setting audio session category to "Playback" (since iOS 6) */
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    NSError *setCategoryError = nil;
+    BOOL ok = [audioSession setCategory:AVAudioSessionCategoryPlayback error:&setCategoryError];
+    if (!ok) {
+        NSLog(@"Error setting AVAudioSessionCategoryPlayback: %@", setCategoryError);
+    };
+
     if (self.childBrowser == nil) {
 #if __has_feature(objc_arc)
         self.childBrowser = [[ChildBrowserViewController alloc] initWithScale:NO];
